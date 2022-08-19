3dea686bc294f898d06c9f4778bba6fab7df5c4c
dev-emti <dev.emti@gmail.com>
2022-08-19 18:02:19 +0800

    mobile

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+togglerMenu = document.querySelector('.mobile-nav-toggle');
+nav = document.querySelector('#nav');
+overlay = document.querySelector(".overlay");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+togglerMenu.addEventListener("click", (e) => {
+    visibility = nav.getAttribute('data-visible')
 
-setupCounter(document.querySelector('#counter'))
+    if(visibility === "false"){
+        nav.setAttribute('data-visible', true)
+        overlay.setAttribute('data-visible', true)
+        togglerMenu.setAttribute('aria-expanded', true)
+    }else{
+        nav.setAttribute('data-visible', false)
+        overlay.setAttribute('data-visible', false)
+        togglerMenu.setAttribute('aria-expanded', false)
+    }
+});
+
+// DROPDOWN
+features = document.querySelector('#features');
+dropdownItems = document.querySelector('.dropdown-items');
+featuresDropIcon = document.querySelector('#features-drop-icon');
+
+features.addEventListener("click", (e) => {
+    visibility = dropdownItems.getAttribute('data-visible')
+
+    if(visibility === "false"){
+        dropdownItems.setAttribute('data-visible', true)
+        featuresDropIcon.setAttribute('data-active', true)
+    }else{
+        dropdownItems.setAttribute('data-visible', false)
+        featuresDropIcon.setAttribute('data-active', false)
+    }
+});
